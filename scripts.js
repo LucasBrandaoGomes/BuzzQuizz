87cf3d54ab2fc90325a338110d3261745a40cd22
GabiProg <gabrielaterese07@gmail.com>
2022-04-25 10:31:36 -0300

    Logica das respostas em andamento - Gabi

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let quantasPerguntas = [];
+
+
 let titulo;
 let imagem;
 let pergunta;
@@ -80,24 +83,112 @@ function acharQuizz(){
 const oQuizz = quizzes.filter(acharQuizz)
 console.log(oQuizz)
 
-//tela3-gabs
+/*tela3-gabs*/
+let qtdPerguntas;
 function camposPreenchidos(){
     titulo = document.querySelector(".titulo").querySelector("input");
     imagem = document.querySelector(".imagem").querySelector("input");
-    pergunta = document.querySelector(".pergunta").querySelector("input");
+    pergunta = document.querySelector(".quantidadePerguntas").querySelector("input");
     nivel = document.querySelector(".nivel").querySelector("input");
     botao = document.querySelector("button");
     if (((titulo.value !== "") && (titulo.value.length >= 20)) && (imagem.value !== "") && ((pergunta.value !=="") && (Number(pergunta.value) >= 3)) && ((nivel.value !== "") && (Number(nivel.value) >= 2))){
-        console.log("botao funciona");
+        const tela3 = document.querySelector(".tela3");
+        const tela4 = document.querySelector(".tela4");
+        qtdPerguntas = Number(pergunta.value);
+        setTimeout(() => trocarDeTela(tela3,tela4) , 500);
+    } else {
+        alert("Preencha os dados corretamente!");
+    }
+    entradaPerguntas();
+}
+/*TELA 4 - GABS*/
+adcPergunta();
+entradaPerguntas();
+
+function entradaPerguntas(){
+    qtdPerguntas = Number(pergunta.value);
+    for(let i = 1; i < qtdPerguntas; i++){
+        let qtd = adcPergunta(i);
+        quantasPerguntas.push(qtd);
+    }
+}
+
+function adcPergunta(indice){
+   let totalPerguntas = document.querySelector(".perguntasRestantes");
+   totalPerguntas.innerHTML += `
+                                <div class="escondePergunta" onclick="infoPerguntas(this); camposPreenchidosPerguntas()">
+                                    <div class="pergunta2">
+                                        <h2>Pergunta</h2>
+                                        <ion-icon name="create-outline"></ion-icon>
+                                    </div>
+                                </div>`
+}
+function infoPerguntas(elemento){
+    elemento.classList.add("escondido");
+
+    let form2 = document.querySelector(".formulario2");
+
+    form2.innerHTML +=`<div class="formulario2-1">
+    <h3 class="pergunta1">Pergunta</h3>
+    <form class="pergunta1texto">
+        <input type="text" placeholder="Texto da pergunta">
+    </form>
+    <form class="pergunta1cor">
+        <input type="text" placeholder="Cor de fundo da pergunta">
+    </form>
+</div>
+<div class="formulario2-2">
+    <h3 class="respostaCorreta">Resposta correta</h3>
+    <form class="respostaTexto">
+        <input type="text" placeholder="Resposta correta">
+    </form>
+    <form class="imagemCorreta">
+        <input type="url" name="url" id="url" placeholder="URL da imagem" pattern="https://.*" required>
+    </form>
+</div>
+<div class="formulario2-3">
+    <h3 class="respostasIncorreta">Respostas incorretas</h3>
+    <form class="incorreta1">
+        <input type="text" placeholder="Resposta incorreta 1">
+    </form>
+    <form class="imagemIncorreta1">
+        <input type="url" name="url" id="url" placeholder="URL da imagem 1" pattern="https://.*" required>
+    </form>
+    <form class="incorreta2">
+        <input type="url" name="url" id="url" placeholder="Resposta incorreta 2">
+    </form>
+    <form class="imagemIncorreta2">
+        <input type="text" placeholder="URL da imagem 2" pattern="https://.*" required>
+    </form>
+    <form class="incorreta3">
+        <input type="text" placeholder="Resposta incorreta 3">
+    </form>
+    <form class="imagemIncorreta3">
+        <input type="url" name="url" id="url" placeholder="URL da imagem 3" pattern="https://.*" required>
+    </form>
+</div>` 
+}
+
+function camposPreenchidosPerguntas(){
+    let textoPergunta = document.querySelector(".pergunta1texto input");
+    let corPergunta = document.querySelector(".pergunta1cor input");
+    let repostaCerta = document.querySelector(".respostaTexto input");
+    let imgRespostaCerta = document.querySelector(".imagemCorreta input");
+    let resptIncorreta1 = document.querySelector(".incorreta1 input");
+    let imgIncorreta1 = document.querySelector(".imagemIncorreta1 input");
+    let resptIncorreta2 = document.querySelector(".incorreta2 input");
+    let imgIncorreta2 = document.querySelector(".imagemIncorreta2 input");
+    let resptIncorreta3 = document.querySelector(".incorreta3 input");
+    let imgIncorreta3 = document.querySelector(".imagemIncorreta3 input");
+
+    if((textoPergunta.value.length >= 20) && (corPergunta.value !== "") && (repostaCerta.value !== "") && 
+    (imgRespostaCerta.value !== "") && (resptIncorreta1.value !== "") && (imgIncorreta1.value !== "") && 
+    (resptIncorreta2.value !== "") && (imgIncorreta2.value !== "") && (resptIncorreta3.value !== "") && (imgIncorreta3.value !== "")){
+        console.log("botão funciona")
     } else {
         alert("Preencha os dados corretamente!");
     }
 }
-
-
-
-
-
 
 
 
